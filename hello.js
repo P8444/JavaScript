dda2a4051c43e2c41e9a33e5c0b8d90ac09b9a11
P8444 <pwaghmare68@gmail.com>
2023-05-06 20:00:26 +0530

    Javascript
    Javascript

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Poonam Waghmare");
+var firstName="Poonam"
+console.log(firstName);
+var age;    //variable declaration
+age="30";   //var initialization
+console.log(age);
+
+
+var city = "Satara"
+console.log(city);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "Satara"; // Variable Initialization
+console.log(holder);
+
